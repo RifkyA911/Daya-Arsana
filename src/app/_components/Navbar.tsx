66a3ed8f9e2ece5b80428c63a49ca6ef145f8866
Rifky Akhmad F <38658854+RifkyA911@users.noreply.github.com>
2025-12-10 21:57:15 +0700

    up: navbar buttons

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -5,20 +5,20 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="py-2 px-4 lg:px-12 bg-background font-sans">
-      <div className="max-w-8xl mx-auto">
+      <div className="xmax-w-8xl mx-auto">
         <div className="flex flex-row items-center justify-between">
           <div className="flex flex-row gap-4 xbg-red-400 w-full">
             <h2 className="text-3xl md:text-4xl font-semibold">
               D-A
             </h2>
             <div className="xbg-amber-100 flex flex-1 flex-row justify-center items-center gap-4">
-              <Link href="/" className="text-sm hover:text-foreground transition-colors p-4 border border-amber-400 text-amber-400">
+              <Link href="/" className="text-sm hover:text-white transition-colors px-4 py-3 hover:bg-orange-400 border border-orange-400 text-orange-400">
                 Home
               </Link>
-              <Link href="#features" className="text-sm hover:text-foreground transition-colors p-4 border  border-amber-400 text-amber-400">
+              <Link href="#features" className="text-sm hover:text-white transition-colors px-4 py-3 hover:bg-orange-400 border border-orange-400 text-orange-400">
                 Service
               </Link>
-              <Link href="#partnership" className="text-sm hover:text-foreground transition-colors p-4 border  border-amber-400 text-amber-400">
+              <Link href="#partnership" className="text-sm hover:text-white transition-colors px-4 py-3 hover:bg-orange-400 border border-orange-400 text-orange-400">
                 About
               </Link>
             </div>
