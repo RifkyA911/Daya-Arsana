0861d89bc1aa5b282cd2a78b29a4c61ceec51ead
Rifky Akhmad F <38658854+RifkyA911@users.noreply.github.com>
2025-12-11 21:50:35 +0700

    up: adjust footer layout

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -2,35 +2,42 @@
 
 const Footer = () => {
   return (
-    <footer id="footer" className="py-16 px-6 lg:px-12 bg-background border-y border-border">
+    <footer id="footer" className="py-8 px-6 lg:px-12 bg-background border-y border-border">
       <div className="xmax-w-7xl mx-auto">
-        <div className="md:text-2xl grid lg:grid-cols-4 gap-12 mb-16">
-          <div className="lg:col-span-2">
-            <h3 className="md:text-7xl mb-4">
-              ACCELERATE GROWTH,
-              MAXIMIZE YOUR ROI
-            </h3>
+        <div className="md:text-2xl grid lg:grid-cols-12 gap-12 mb-16">
+          <div className="lg:col-span-8">
+            <div className="lg:max-w-[800px]">
+
+              <h3 className="md:text-7xl mb-4">
+                ACCELERATE GROWTH,
+                MAXIMIZE YOUR ROI
+              </h3>
+            </div>
           </div>
 
-          <div>
-            <h4 className="font-semibold tracking-wider text-muted-foreground mb-4">
+          <div className="lg:col-span-2 text-[20px]">
+            <h4 className="text-[24px] tracking-wider mb-2">
               Address
             </h4>
-            <p className="text-muted-foreground leading-relaxed">
+            <p className="leading-relaxed">
               Gang Salak, RT.001/RW.002, Krajan, Klampok, Kec. Singosari, Kabupaten Malang, Jawa Timur 65153
             </p>
-            <p className="text-muted-foreground mt-4">
+            <h4 className="mt-4 text-[24px] tracking-wider">
+              Contact
+            </h4>
+            <p className="mt-2">
               hello@daya-arsana.com<br />
               (62) 851 - 5539 - 4885
             </p>
           </div>
 
-          <div>
-            <h4 className="font-semibold tracking-wider text-muted-foreground mb-4">
+          <div className="lg:col-span-2 text-[20px]">
+            {/* <h4 className="font-semibold tracking-wider mb-4">
               SOCIALS
-            </h4>
-            <ul className="space-y-2 text-muted-foreground">
+            </h4> */}
+            <ul className="flex flex-col gap-4 font-semibold">
               <li className="hover:text-foreground transition-colors cursor-pointer">Instagram</li>
+              <li className="hover:text-foreground transition-colors cursor-pointer">Behance</li>
               <li className="hover:text-foreground transition-colors cursor-pointer">Twitter</li>
               <li className="hover:text-foreground transition-colors cursor-pointer">LinkedIn</li>
               <li className="hover:text-foreground transition-colors cursor-pointer">Dribbble</li>
