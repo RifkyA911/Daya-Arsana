85699cd4c4d97dfae8ca1a8565dc06aad8102bcf
Rifky Akhmad F <38658854+RifkyA911@users.noreply.github.com>
2025-12-13 17:28:53 +0700

    fix: footer layout

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -2,46 +2,44 @@
 
 const Footer = () => {
   return (
-    <footer id="footer" className="py-8 px-6 lg:px-12 bg-background border-y border-border">
+    <footer id="footer" className="py-8 px-8 lg:px-28 bg-background border-y border-black">
       <div className="xmax-w-7xl mx-auto">
-        <div className="md:text-2xl grid lg:grid-cols-12 gap-12 mb-16">
-          <div className="lg:col-span-8">
-            <div className="lg:max-w-[800px]">
-
-              <h3 className="md:text-7xl mb-4">
-                ACCELERATE GROWTH,
-                MAXIMIZE YOUR ROI
-              </h3>
+        <div className="flex flex-col md:flex-row w-full gap-12 mb-16 justify-between md:text-2xl ">
+          <div className="lg:max-w-[800px]">
+            <h3 className="text-[40px] md:text-7xl 2k:text-7xl font-medium">
+              ACCELERATE GROWTH,
+              MAXIMIZE YOUR ROI
+            </h3>
+          </div>
+          <div className="grid grid-cols-12 gap-8 flex-1 justify-items-end">
+            <div className="col-span-8 2k:col-span-4 2k:col-start-5 text-[20px]">
+              <h4 className="md:text-[24px] tracking-wider mb-2 ">
+                Address
+              </h4>
+              <p className="leading-relaxed">
+                Gang Salak, RT.001/RW.002, Krajan, Klampok, Kec. Singosari, Kabupaten Malang, Jawa Timur 65153
+              </p>
+              <h4 className="mt-4 md:text-[24px] tracking-wider">
+                Contact
+              </h4>
+              <p className="mt-2 leading-relaxed">
+                hello@daya-arsana.com<br />
+                (62) 851 - 5539 - 4885
+              </p>
             </div>
-          </div>
 
-          <div className="lg:col-span-2 text-[20px]">
-            <h4 className="text-[24px] tracking-wider mb-2">
-              Address
-            </h4>
-            <p className="leading-relaxed">
-              Gang Salak, RT.001/RW.002, Krajan, Klampok, Kec. Singosari, Kabupaten Malang, Jawa Timur 65153
-            </p>
-            <h4 className="mt-4 text-[24px] tracking-wider">
-              Contact
-            </h4>
-            <p className="mt-2">
-              hello@daya-arsana.com<br />
-              (62) 851 - 5539 - 4885
-            </p>
-          </div>
-
-          <div className="lg:col-span-2 text-[20px]">
-            {/* <h4 className="font-semibold tracking-wider mb-4">
+            <div className="col-span-4 2k:col-span-2 2k:col-start-10 text-[20px]">
+              {/* <h4 className="font-semibold tracking-wider mb-4">
               SOCIALS
             </h4> */}
-            <ul className="flex flex-col gap-4 font-semibold">
-              <li className="hover:text-foreground transition-colors cursor-pointer">Instagram</li>
-              <li className="hover:text-foreground transition-colors cursor-pointer">Behance</li>
-              <li className="hover:text-foreground transition-colors cursor-pointer">Twitter</li>
-              <li className="hover:text-foreground transition-colors cursor-pointer">LinkedIn</li>
-              <li className="hover:text-foreground transition-colors cursor-pointer">Dribbble</li>
-            </ul>
+              <ul className="flex flex-col gap-4 md:gap-6 font-medium">
+                <li className="hover:text-foreground transition-colors cursor-pointer">Instagram</li>
+                <li className="hover:text-foreground transition-colors cursor-pointer">Behance</li>
+                <li className="hover:text-foreground transition-colors cursor-pointer">Twitter</li>
+                <li className="hover:text-foreground transition-colors cursor-pointer">LinkedIn</li>
+                <li className="hover:text-foreground transition-colors cursor-pointer">Dribbble</li>
+              </ul>
+            </div>
           </div>
         </div>
 
